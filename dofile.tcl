04fb778bed7640995706ad899bee7902c8df1fd1
Karim Mahmoud <82693464+karimmahmoud22@users.noreply.github.com>
2022-12-31 00:45:31 +0200

    rerun the test cases after fixing issues

--- dofile.tcl
+++ dofile.tcl
@@ -35,7 +35,7 @@ sim:/phase_3/ftch/i_branch_decision
 add wave -position insertpoint sim:/phase_3/hazard_unit/*
 force -freeze sim:/phase_3/i_clk 1 0, 0 {50 ps} -r 100
 force -freeze sim:/phase_3/i_reset 1 0
-force -freeze sim:/phase_3/i_interrupt 1 0
+force -freeze sim:/phase_3/i_interrupt 0 0
 run 125
 force -freeze sim:/phase_3/i_reset 0 0
 run 75
